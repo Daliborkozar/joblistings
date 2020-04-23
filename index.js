1ff0fffda976351bdb0146d371d75211577c21b6
dalibor kozar <kozardalibor@gmail.com>
2020-04-23 18:52:40 +0200

    code modify to async 2

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 
 const contentContainer = document.getElementById('container');
+const filters = document.getElementById('filters');
+const filterBar = document.getElementById('filterbar');
+let filterList = []
 
  async function getData(){
     try {
@@ -14,54 +17,101 @@ const contentContainer = document.getElementById('container');
 };
 
 
-const addJobsToDOM = data => {
+// const addJobsToDOM = data => {
   
-    if (Array.isArray(data) && data.length > 0) {
-      data.map(data => {
-        renderJobs(data);
-      });
-    } else if (data) {
-      renderJobs(data);
-    }
-  };
+//     if (Array.isArray(data) && data.length > 0) {
+//       data.map(data => {
+//         renderJobs(data);
+//       });
+//     } else if (data) {
+//       renderJobs(data);
+//     }
+//   };
 
-const renderJobs = item => {
-    const infoNew = item.new ? `<div class="tag bg-light">New!</div>` : '';
-    const infoFeatured = item.featured ? `<div class="tag bg-dark">Featured</div>` : '';
-    const itemFilters = [item.role, item.level, ...(item.languages || []), ...(item.tools || [])];
-    let filterContent = ''
-    itemFilters.forEach((filter) => {
-      filterContent += `<div class="filter" onclick="filter(this)" data-value="${filter}">${filter}</div>`;
-    });
-    const markup =`<div class="card">
-         <img src="${item.logo}" alt="logo-${item.company}"/>
-         <div class="grid-item">
-           <div class="info">
-             <div class="info-heading">
-               <h6>${item.company}</h6>
-               ${infoNew}
-               ${infoFeatured}
-             </div>
-             <h4>${item.position}</h4>
-             <div class="info-status">
-               <div class="status">${item.postedAt} &centerdot;
-               </div>
-               <div class="status">${item.contract} &centerdot;
-               </div>
-               <div class="status">${item.location}</div>
-             </div>
-           </div>
-           <hr class="divider">
-           <div class="filters">
-             ${filterContent}
-           </div>
-         </div>
-       </div>`
-    contentContainer.insertAdjacentHTML('beforeend', markup)
+function checkFilter(compareList) {
+  return filterList.every((el) => compareList.indexOf(el) >= 0);
+}
+ 
+
+async function renderJobs() {
+    
+    const data = await getData();
+    data.map(item => { 
+      const infoNew = item.new ? `<div class="tag bg-light">New!</div>` : '';
+      const infoFeatured = item.featured ? `<div class="tag bg-dark">Featured</div>` : '';
+      const itemFilters = [item.role, item.level, ...(item.languages || []), ...(item.tools || [])];
+      if (filterList.length === 0 || checkFilter(itemFilters)) {
+      let filterContent = ''
+      itemFilters.map((filter) => {
+        filterContent += `<div class="filter" onclick="filter(this)" data-value="${filter}">${filter}</div>`;
+        })
+      
+      const markup =`<div class="card">
+          <img src="${item.logo}" alt="logo-${item.company}"/>
+          <div class="grid-item">
+            <div class="info">
+              <div class="info-heading">
+                <h6>${item.company}</h6>
+                ${infoNew}
+                ${infoFeatured}
+              </div>
+              <h4>${item.position}</h4>
+              <div class="info-status">
+                <div class="status">${item.postedAt} &centerdot;
+                </div>
+                <div class="status">${item.contract} &centerdot;
+                </div>
+                <div class="status">${item.location}</div>
+              </div>
+            </div>
+            <hr class="divider">
+            <div class="filters">
+              ${filterContent}
+            </div>
+          </div>
+        </div>`
+      contentContainer.insertAdjacentHTML('beforeend', markup)
+      }
+  })
 }
 
-const main = async () => {
-    addJobsToDOM(await getData());
-  };
+function renderFilterBar() {
+  if (filterList.length === 0) {
+    filterBar.classList.remove('show');
+    filterBar.classList.add('hide');
+  } else {
+    filterBar.classList.remove('hide');
+    filterBar.classList.add('show');
+  }
+  const filterBarContent = filterList.map(
+    (filter) =>
+      `<div class="filter-btn" data-value="${filter}" onclick="filter(this)"><p>${filter}</p><div><i class="fas fa-times"></i></div></div>`
+  );
+  filters.innerHTML = '';
+  filterBarContent.forEach((item) => {
+    filters.innerHTML += item;
+  });
+  renderJobs();
   
-  main()
+}
+
+function filter(element) {
+  const filterValue = element.getAttribute('data-value');
+
+  if (filterList.includes(filterValue)) {
+    filterList = filterList.filter((filter) => filter !== filterValue);
+    renderFilterBar();
+  } else {
+    filterList.push(filterValue);
+    renderFilterBar();
+  }
+}
+
+document.getElementById('clear').addEventListener('click', () => {
+  filterList = [];
+  renderFilterBar();
+});
+
+(function () {
+ renderJobs();
+})();
